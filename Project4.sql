d881eda841c9043d27085fd4d13e30cf96b138a1
Muhammet Can Namlı <78431281+MuhammetCanNamli@users.noreply.github.com>
2024-03-08 15:10:17 +0300

    Update Project4.sql

--- Project4.sql
+++ Project4.sql
@@ -1,26 +1,26 @@
-- List the different values in the replacement_cost column in the movie table.
+-- List the different values in the replacement_cost column in the movie table.
 
 SELECT DISTINCT replacement_cost
 FROM film;
 
-- How many different data are there in the replacement_cost column in the movie table?
+-- How many different data are there in the replacement_cost column in the movie table?
 
 SELECT COUNT(DISTINCT replacement_cost)
 FROM film;
 
-- How many of the movie titles in the movie table start with the character T and also have a rating equal to 'G'?
+-- How many of the movie titles in the movie table start with the character T and also have a rating equal to 'G'?
 
 SELECT COUNT(*)
 FROM film
 WHERE title LIKE 'T%' AND rating = 'G';
 
-- How many of the country names in the country table consist of 5 characters?
+-- How many of the country names in the country table consist of 5 characters?
 
 SELECT COUNT(*)
 FROM country
 WHERE country LIKE '_____';
 
-- How many of the city names in the city table end with 'R' or the character r?
+-- How many of the city names in the city table end with 'R' or the character r?
 
 SELECT COUNT(*)
 FROM city
